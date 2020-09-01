4d8745c187a2a5c046a0e68ca817144d5e887c4f
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-09-01 10:09:14 +0700

    Finish Weather Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import WeatherCard from './WeatherCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <WeatherCard />
     </div>
   );
 }
